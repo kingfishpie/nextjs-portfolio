295e67e755e942fe5d1f0363daa587cc3118a9dc
Ryan MacInnes <ryan.macinnes@gmail.com>
2021-02-19 17:40:42 -0800

    Dynamic favicon.

--- components/meta.js
+++ components/meta.js
@@ -1,10 +1,18 @@
 import Head from 'next/head'
+import useSystemTheme from 'react-use-system-theme'
 
-const Meta = ({ title }) => <Head>
-  <title>{title}</title>
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-</Head>
+const Meta = ({ title }) => {
+  const theme = useSystemTheme() || 'light'
+
+  return(
+    <Head>
+      <title>{title}</title>
+      <link rel="apple-touch-icon" sizes="180x180" href={`/apple-touch-icon-${theme}.png`} />
+      <link rel="icon" type="image/png" sizes="32x32" href={`/favicon-32x32-${theme}.png`} />
+      <link rel="icon" type="image/png" sizes="16x16" href={`/favicon-16x16-${theme}.png`} />
+      <link rel="shortcut icon" href={`/favicon-${theme}.ico`} />
+    </Head>
+  )
+}
 
 export default Meta
